4e2f63cb06c6980a9d141187d282b2dccc43d612
Kjartan Dige <emildige@gmail.com>
2026-02-17 17:18:06 +0100

    setup tanstack router

---
+++ src/routes/__root.tsx
@@ -0,0 +1,11 @@
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+
+const RootLayout = () => (
+  <>
+    <div className="p-2 flex gap-2"></div>
+    <hr />
+    <Outlet />
+  </>
+);
+
+export const Route = createRootRoute({ component: RootLayout });
