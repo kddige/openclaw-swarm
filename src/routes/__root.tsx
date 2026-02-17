19f11dd55810cb16d17243d33a205739d8c9402e
Kjartan Dige <emildige@gmail.com>
2026-02-17 17:20:45 +0100

    Add Tailwind CSS and plugin, update styles and config

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,12 @@
+import '../index.css'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 
-const RootLayout = () => (
-  <>
-    <div className="p-2 flex gap-2"></div>
-    <hr />
-    <Outlet />
-  </>
-)
-
 export const Route = createRootRoute({ component: RootLayout })
+
+function RootLayout() {
+  return (
+    <>
+      <Outlet />
+    </>
+  )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'node:path'
 import electron from 'vite-plugin-electron/simple'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
       autoCodeSplitting: false,
     }),
     react(),
+    tailwindcss(),
     electron({
       main: {
         // Shortcut of `build.lib.entry`.
